ab1e94f0d3344e469fd39a874c6fdb51e190fc51
Dennis Traub <dennis.traub@gmail.com>
2012-09-12 11:58:58 +0200

    Got first test working

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -4,18 +4,14 @@
 var server = require("./server.js");
 var http = require("http");
 
-exports.tearDown = function(callback) {
+exports.tearDown = function(done) {
 	server.stop(function() {
-		callback();
+		done();
 	});
 };
 
-// TODO: Handle case where stop() is called before start()
-// TODO: test-drive stop() callback
-
 exports.testHttpServer = function(test) {
 	server.start();
-
 	http.get("http://localhost:8080", function(response) {
 		test.done();
 	});
